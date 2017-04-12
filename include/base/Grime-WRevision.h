c2def6a643032fd55ed1112c6499f7978db3a1df
Michael Short <hereiam@mit.edu>
2017-04-12 08:28:48 -0400

    Updated syntax to current MOOSE snapshot and changed name to Geminio throughout

--- include/base/Grime-WRevision.h
+++ include/base/Grime-WRevision.h
@@ -3,6 +3,6 @@
 #ifndef GRIME-W_REVISION_H
 #define GRIME-W_REVISION_H
 
-#define GRIME-W_REVISION "git commit 73ec8c8 on 2017-04-08"
+#define GRIME-W_REVISION "git commit 9762261 on 2017-04-08"
 
 #endif // GRIME-W_REVISION_H
